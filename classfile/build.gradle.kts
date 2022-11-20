65b8a633ef945c4535abf3ab1d9d5f3114f33b5b
netomi <thomas.neidhart@gmail.com>
2022-11-20 16:55:07 +0100

    Remove tests that require Java 17.

--- classfile/build.gradle.kts
+++ classfile/build.gradle.kts
@@ -6,11 +6,6 @@ base {
     archivesName.set("bat-classfile")
 }
 
-tasks.compileTestJava {
-    sourceCompatibility = JavaVersion.VERSION_17.toString()
-    targetCompatibility = JavaVersion.VERSION_17.toString()
-}
-
 dependencies {
     implementation(projects.common)
     testImplementation(kotlin("test"))
